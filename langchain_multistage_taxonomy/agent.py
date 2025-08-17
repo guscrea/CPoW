ac14d1ac8bc7110ce2a6211c6d9c8b365273c77c
cloud-machine-111 <ran_zhao@brown.edu>
2025-08-16 20:34:11 -0700

    update workflow to integrate new nodes

--- langchain_multistage_taxonomy/agent.py
+++ langchain_multistage_taxonomy/agent.py
@@ -11,13 +11,16 @@ workflow = StateGraph(OverallState)
 
 # Add nodes:
 workflow.add_node('seg_by_superclaim', seg_by_superclaim)
+workflow.add_node('emit_segments", emit_segments)
 workflow.add_node('seg_by_subclaims', seg_by_subclaims)
+workflow.add_node('score_segment', score_segment)
 workflow.add_node('aggregate_jsons', aggregate_jsons)
 # Add edges:
 workflow.add_edge(START, 'seg_by_superclaim')
 workflow.add_conditional_edges('seg_by_superclaim', emit_segments, ['seg_by_subclaims']) # syntax reference from Medium: graph.add_conditional_edges("evaluate_solution", continue_to_deep_thought, ["deepen_thought"])
-
-workflow.add_edge('seg_by_subclaims', END)
+workflow.add_edge('seg_by_subclaims', 'score_segment')
+workflow.add_edge('score_segment', 'aggregate_jsons') # TODO: this is placeholder code! aggregate must actually aggregate.
+workflow.add_edge('aggregate_jsons', END)
 
 # Compile:
 graph = workflow.compile() 
